e624701f7229b8e044aaf5bcf000f8a554df73b0
Abdullrahman almaghrabi <73081830+abdprince@users.noreply.github.com>
2026-03-06 03:29:37 +0300

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Transfer from '../views/Transfer.vue'
+import Rewards from '../views/Rewards.vue'
+import Tasks from '../views/Tasks.vue'
+import Referrals from '../views/Referrals.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  }
+  { path: '/', name: 'Home', component: Home },
+  { path: '/transfer', name: 'Transfer', component: Transfer },
+  { path: '/rewards', name: 'Rewards', component: Rewards },
+  { path: '/tasks', name: 'Tasks', component: Tasks },
+  { path: '/referrals', name: 'Referrals', component: Referrals }
 ]
 
 const router = createRouter({
