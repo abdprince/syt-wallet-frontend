34c296bd9cf8174602005db94c981f1657cc2b0f
Abdullrahman almaghrabi <73081830+Abdomghrbi@users.noreply.github.com>
2026-03-07 05:36:48 +0300

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import { createStore } from 'vuex'
 import axios from 'axios'
 
-const API_URL = 'https://syt-wallet-v2.vercel.app/api'
+const API_URL = 'https://syt-wallet-v2.onrender.com/api'
 
 export default createStore({
   state: {
